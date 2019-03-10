61a116caa19dccb0f23123d0dce4da20c393a906
Labais <reinitto@gmail.com>
2019-03-10 21:04:46 +0100

    init commit

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+app.use("/public", express.static(process.cwd() + "/public"));
+
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname + "/public/index.html"));
+});
+app.listen(3000);
